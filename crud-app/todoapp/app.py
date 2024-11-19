6c47e97fd5a37782ff5801034424409634d1b48f
msharma1 <manish.sharma0201cs@gmail.com>
2024-11-19 23:19:26 +0000

    Implement checklist todo when ticked sets the complete field to true and renders the completed items in future requets

--- crud-app/todoapp/app.py
+++ crud-app/todoapp/app.py
@@ -38,9 +38,23 @@ def create_todo():
   else:
     return jsonify(body)
 
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+  try:
+    completed = request.get_json()['completed']
+    print('completed', completed)
+    todo = Todo.query.get(todo_id)
+    todo.completed = completed
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return redirect(url_for('index'))
+
 @app.route('/')
 def index():
-  return render_template('index.html', data=Todo.query.all())
+  return render_template('index.html', todos=Todo.query.all())
 
 if __name__ == '__main__':
    app.run(host="0.0.0.0", port=3000)
